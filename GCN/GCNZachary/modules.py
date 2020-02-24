4e3a5baa44efe26d440bf5db3a13cb3ffd41174a
ptdang1001 <60625154+ptdang1001@users.noreply.github.com>
2020-02-24 17:34:17 -0500

    Update modules.py

--- GCN/GCNZachary/modules.py
+++ GCN/GCNZachary/modules.py
@@ -13,5 +13,8 @@ X:feature matrix (Embeding H0)
 W:weight matrix
 '''
 def gcn_layer(A_hat, D_hat, X, W):
-    return relu(D_hat**-1 * A_hat * X * W)
-
+    return relu(D_hat**-1 * A_hat * X * W)  
+'''
+We use H_i+1 = D_hat^-1 * A_hat * H_i * W (H0=X) here it means we just normalize the rows. There is another one which is more
+complexity H_i+1 = D_hat^-1 * A_hat * D_hat^-1 * H_i * W (H0=X) it can normalize the rows and cols.
+'''
